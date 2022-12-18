d7cb191f8d5e42c0908ed25518005e3ae1e42e87
MahdiUddinAhmed <mahdi.uddin.ahmed@g.bracu.ac.bd>
2022-12-19 02:23:06 +0600

    Add existing project files to Git

---
+++ Client/src/data.js
@@ -0,0 +1,136 @@
+
+export const sliderItems = [{
+    id: 1,
+    img: "https://i.ibb.co/SX7H40V/MHA.jpg",
+    title: "MHA CHAPTER 368",
+    desc: "Read Latest Chapter Today!",
+    bg: "515d67",
+}, {
+    id: 2,
+    img: "https://i.ibb.co/QvWkNgF/2.jpg",
+    title: "BATMAN Year One",
+    desc: "READ THE ORIGIN STORY OF THE INFAMOUS MASKES CRUSADER",
+    bg: "4d0000",
+}, {
+    id: 3,
+    img: "https://i.ibb.co/N3fYYSx/4.jpg",
+    title: "READ VILLAINESS IS A MARIONATTE CHAPTER 69",
+    desc: "Read the survival story of a Lone reincarnator as she works brilliantly to avoid a tragic death!",
+    bg: "b88ca9",
+}, ];
+
+export const categories = [{
+        id: 1,
+        img: "https://i.ibb.co/kcD1xG0/m.jpg",
+        title: "MANGA",
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/L6ryq75/c.jpg",
+        title: "COMIC BOOKS",
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/thdwmvq/3.jpg",
+        title: "WEBTOONS!",
+    },
+];
+
+export const popularProducts = [{
+        id: 1,
+        img: "https://i.ibb.co/LQNdD1z/kj8.jpg",
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/SX7H40V/MHA.jpg",
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/sgY3tbn/OP.jpg",
+    },
+    {
+        id: 4,
+        img: "https://i.ibb.co/565RshW/sm.jpg",
+    },
+    {
+        id: 5,
+        img: "https://i.ibb.co/bsQPf41/4.jpg",
+    },
+    {
+        id: 6,
+        img: "https://i.ibb.co/4Zt7k4S/cw.jpg",
+    },
+    {
+        id: 7,
+        img: "https://i.ibb.co/QvWkNgF/2.jpg",
+    },
+    {
+        id: 8,
+        img: "https://i.ibb.co/jW03Yg6/Infinity-Gauntlet-1.jpg",
+    }
+]
+
+export const AllProducts = [{
+        id: 1,
+        img: "https://i.ibb.co/LQNdD1z/kj8.jpg",
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/SX7H40V/MHA.jpg",
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/sgY3tbn/OP.jpg",
+    },
+    {
+        id: 4,
+        img: "https://i.ibb.co/565RshW/sm.jpg",
+    },
+    {
+        id: 5,
+        img: "https://i.ibb.co/bsQPf41/4.jpg",
+    },
+    {
+        id: 6,
+        img: "https://i.ibb.co/4Zt7k4S/cw.jpg",
+    },
+    {
+        id: 7,
+        img: "https://i.ibb.co/QvWkNgF/2.jpg",
+    },
+    {
+        id: 8,
+        img: "https://i.ibb.co/jW03Yg6/Infinity-Gauntlet-1.jpg",
+    }, {
+        id: 9,
+        img: "https://i.ibb.co/LQNdD1z/kj8.jpg",
+    },
+    {
+        id: 10,
+        img: "https://i.ibb.co/SX7H40V/MHA.jpg",
+    },
+    {
+        id: 11,
+        img: "https://i.ibb.co/sgY3tbn/OP.jpg",
+    },
+    {
+        id: 12,
+        img: "https://i.ibb.co/565RshW/sm.jpg",
+    },
+    {
+        id: 13,
+        img: "https://i.ibb.co/bsQPf41/4.jpg",
+    },
+    {
+        id: 14,
+        img: "https://i.ibb.co/4Zt7k4S/cw.jpg",
+    },
+    {
+        id: 15,
+        img: "https://i.ibb.co/QvWkNgF/2.jpg",
+    },
+    {
+        id: 16,
+        img: "https://i.ibb.co/jW03Yg6/Infinity-Gauntlet-1.jpg",
+    }
+]
